d129fa8b7a695973c3a73635941a4793156d4e3c
MateoGarcesUNL <mgarces2@huskers.unl.edu>
2025-08-28 12:59:30 -0500

    initial commit

--- hello.c
+++ hello.c
@@ -14,7 +14,6 @@ int main(int argc, char **argv) {
   printf("\n");
   printf("Mateo Garces, Computer Science\n");
   printf("I love the School of Computing\n");
-  printf("I love the School of Computing\n");
   printf("\n");
 
   return 0;
