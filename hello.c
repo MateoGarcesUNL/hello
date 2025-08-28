27e975439a423b8456592e18ad022e06e7802aac
MateoGarcesUNL <mgarces2@huskers.unl.edu>
2025-08-28 12:40:59 -0500

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Mateo Garces
+ *         mgarces2@huskers.unl.edu
+ * Date: 2026/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("\n");
+  printf("Mateo Garces, Computer Science\n");
+  printf("I love the School of Computing\n");
+  printf("\n");
+
+  return 0;
+}
